a92481f77c0657346225337c93dcd01b97d15f5b
Abner Werley <abnerwerley77@gmail.com>
2023-02-08 21:24:45 -0300

    :satefy_vest: validation: added validation to check if the amount of bonification matches the amount expected

--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -1,6 +1,7 @@
 
 public class TesteReferencias {
 	public static void main(String[] args) {
+		
 		Funcionario gerente = new Gerente();
 		gerente.setSalario(5000.0);
 
@@ -10,10 +11,20 @@ public class TesteReferencias {
 		Funcionario editor = new EditorDeVideo();
 		editor.setSalario(2500.0);
 
+		Funcionario designer = new Designer();
+		designer.setSalario(3000);
+
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(gerente);
 		controle.registra(funcionario);
 		controle.registra(editor);
-		System.out.println(controle.getSoma());
+		controle.registra(designer);
+		
+		double esperado = 6550;
+		if (controle.getSoma() == esperado) {
+			System.out.println("Sucesso, a soma de bonificações bateu com o esperado.");
+		}else {
+			System.out.println("Erro, a soma de bonificações não bateu com o esperado.");
+		}
 	}
 }
